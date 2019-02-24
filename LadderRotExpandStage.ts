34fa7d38f97c8d171a0020bf6f2413143fc25149
Anwesh43 <anweshthecool0@gmail.com>
2019-02-24 23:00:25 +0530

    adding scale methods

--- LadderRotExpandStage.ts
+++ LadderRotExpandStage.ts
@@ -6,3 +6,23 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.9
 const foreColor : string = "#673AB7"
 const backColor : string = "#212121"
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => {
+    return Math.floor(scale / scDiv)
+}
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    return (1 - scaleFactor(scale)) / a + (scaleFactor(scale)) / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
