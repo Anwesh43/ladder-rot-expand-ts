f5e8dab01475941dad41648c1a9954f343af70b4
Anwesh43 <anweshthecool0@gmail.com>
2019-02-25 00:07:10 +0530

    adding LadderRotExpand class

--- LadderRotExpandStage.ts
+++ LadderRotExpandStage.ts
@@ -176,6 +176,30 @@ class LRENode {
             return curr
         }
         cb()
-        return this 
+        return this
+    }
+}
+
+class LadderRotExpand {
+
+    root : LRENode = new LRENode(0)
+    curr : LRENode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
 }
