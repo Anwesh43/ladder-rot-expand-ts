d89782b20905e793a9bf0d3fe275441f6a4103f2
Anwesh43 <anweshthecool0@gmail.com>
2019-02-24 22:39:49 +0530

    adding constants

---
+++ LadderRotExpandStage.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#673AB7"
+const backColor : string = "#212121"
